7b27500c74226f5128e0a0feda0136d21ddc775a
samuell324 <chistov_artem@mail.ru>
2020-10-06 18:36:30 +0300

    add http dependenci, implement basic UI

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,25 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(MainPage());
 }
+
+class MainPage extends StatefulWidget {
+  @override
+  _MainPageState createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Practice 3'),
+        ),
+      ),
+    );
+  }
+}
+
 
