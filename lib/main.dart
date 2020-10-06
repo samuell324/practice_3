3da26750cc49bfef08d9d09056cd32a911e8cbd3
samuell324 <chistov_artem@mail.ru>
2020-10-06 21:58:13 +0300

    convert response into list

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,24 @@
 import 'package:flutter/material.dart';
+import 'dart:convert';
+import 'dart:async';
+import 'package:http/http.dart' as http;
+import 'package:flutter/foundation.dart';
+import 'catData.dart';
 
 void main() {
   runApp(MainPage());
 }
+
+Future<List<CatData>> fetchCats(http.Client client) async {
+  final response = await client.get('https://api.thecatapi.com/v1/breeds/');
+  return compute(parseCat, response.body);
+}
+
+List<CatData> parseCat(responseBody) {
+  final parsed = jsonDecode(responseBody) as Map <String, dynamic>;
+  return parsed[''].map<CatData>((json) => CatData.fromJson(json)).toList();
+}
+
 
 class MainPage extends StatefulWidget {
   @override
