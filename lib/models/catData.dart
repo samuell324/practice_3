c4a236c7d660c4094bd0451bdb67a4c3a721a053
samuell324 <chistov_artem@mail.ru>
2020-10-07 16:59:22 +0300

    rebuild catData model class

--- lib/models/catData.dart
+++ lib/models/catData.dart
@@ -1,49 +1,186 @@
 class CatData {
-  final String id;
-  final String name;
-  final String cfaUrl;
-  final String temperament;
-  final String origin;
-  final String countryCodes;
-  final String countryCode;
-  final String description;
-  final String lifeSpan;
-  final String childFriendly;
+CatData({
+  this.weight,
+  this.id,
+  this.name,
+  this.cfaUrl,
+  this.vetstreetUrl,
+  this.vcahospitalsUrl,
+  this.temperament,
+  this.origin,
+  this.countryCodes,
+  this.countryCode,
+  this.description,
+  this.lifeSpan,
+  this.indoor,
+  this.lap,
+  this.altNames,
+  this.adaptability,
+  this.affectionLevel,
+  this.childFriendly,
+  this.dogFriendly,
+  this.energyLevel,
+  this.grooming,
+  this.healthIssues,
+  this.intelligence,
+  this.sheddingLevel,
+  this.socialNeeds,
+  this.strangerFriendly,
+  this.vocalisation,
+  this.experimental,
+  this.hairless,
+  this.natural,
+  this.rare,
+  this.rex,
+  this.suppressedTail,
+  this.shortLegs,
+  this.wikipediaUrl,
+  this.hypoallergenic,
+  this.catFriendly,
+  this.bidability,
+});
 
-  CatData(
-      this.id,
-      this.name,
-      this.cfaUrl,
-      this.temperament,
-      this.origin,
-      this.countryCodes,
-      this.countryCode,
-      this.description,
-      this.lifeSpan,
-      this.childFriendly);
+Weight weight;
+String id;
+String name;
+String cfaUrl;
+String vetstreetUrl;
+String vcahospitalsUrl;
+String temperament;
+String origin;
+String countryCodes;
+String countryCode;
+String description;
+String lifeSpan;
+int indoor;
+int lap;
+String altNames;
+int adaptability;
+int affectionLevel;
+int childFriendly;
+int dogFriendly;
+int energyLevel;
+int grooming;
+int healthIssues;
+int intelligence;
+int sheddingLevel;
+int socialNeeds;
+int strangerFriendly;
+int vocalisation;
+int experimental;
+int hairless;
+int natural;
+int rare;
+int rex;
+int suppressedTail;
+int shortLegs;
+String wikipediaUrl;
+int hypoallergenic;
+int catFriendly;
+int bidability;
 
-  CatData.fromJson(Map<String, dynamic> json)
-      : id = json['id'],
-        name = json['name'],
-        cfaUrl = json['cfa_url'],
-        temperament = json['temperament'],
-        origin = json['origin'],
-        countryCodes = json['country_codes'],
-        countryCode = json['country_code'],
-        description = json['description'],
-        lifeSpan = json['life_span'],
-        childFriendly = json['child_friendly'];
+factory CatData.fromJson(Map<String, dynamic> json) => CatData(
+weight: Weight.fromJson(json["weight"]),
+id: json["id"],
+name: json["name"],
+cfaUrl: json["cfa_url"] == null ? null : json["cfa_url"],
+vetstreetUrl:
+json["vetstreet_url"] == null ? null : json["vetstreet_url"],
+vcahospitalsUrl:
+json["vcahospitals_url"] == null ? null : json["vcahospitals_url"],
+temperament: json["temperament"],
+origin: json["origin"],
+countryCodes: json["country_codes"],
+countryCode: json["country_code"],
+description: json["description"],
+lifeSpan: json["life_span"],
+indoor: json["indoor"],
+lap: json["lap"] == null ? null : json["lap"],
+altNames: json["alt_names"] == null ? null : json["alt_names"],
+adaptability: json["adaptability"],
+affectionLevel: json["affection_level"],
+childFriendly: json["child_friendly"],
+dogFriendly: json["dog_friendly"],
+energyLevel: json["energy_level"],
+grooming: json["grooming"],
+healthIssues: json["health_issues"],
+intelligence: json["intelligence"],
+sheddingLevel: json["shedding_level"],
+socialNeeds: json["social_needs"],
+strangerFriendly: json["stranger_friendly"],
+vocalisation: json["vocalisation"],
+experimental: json["experimental"],
+hairless: json["hairless"],
+natural: json["natural"],
+rare: json["rare"],
+rex: json["rex"],
+suppressedTail: json["suppressed_tail"],
+shortLegs: json["short_legs"],
+wikipediaUrl:
+json["wikipedia_url"] == null ? null : json["wikipedia_url"],
+hypoallergenic: json["hypoallergenic"],
+catFriendly: json["cat_friendly"] == null ? null : json["cat_friendly"],
+bidability: json["bidability"] == null ? null : json["bidability"],
+);
+
+Map<String, dynamic> toJson() => {
+  "weight": weight.toJson(),
+  "id": id,
+  "name": name,
+  "cfa_url": cfaUrl == null ? null : cfaUrl,
+  "vetstreet_url": vetstreetUrl == null ? null : vetstreetUrl,
+  "vcahospitals_url": vcahospitalsUrl == null ? null : vcahospitalsUrl,
+  "temperament": temperament,
+  "origin": origin,
+  "country_codes": countryCodes,
+  "country_code": countryCode,
+  "description": description,
+  "life_span": lifeSpan,
+  "indoor": indoor,
+  "lap": lap == null ? null : lap,
+  "alt_names": altNames == null ? null : altNames,
+  "adaptability": adaptability,
+  "affection_level": affectionLevel,
+  "child_friendly": childFriendly,
+  "dog_friendly": dogFriendly,
+  "energy_level": energyLevel,
+  "grooming": grooming,
+  "health_issues": healthIssues,
+  "intelligence": intelligence,
+  "shedding_level": sheddingLevel,
+  "social_needs": socialNeeds,
+  "stranger_friendly": strangerFriendly,
+  "vocalisation": vocalisation,
+  "experimental": experimental,
+  "hairless": hairless,
+  "natural": natural,
+  "rare": rare,
+  "rex": rex,
+  "suppressed_tail": suppressedTail,
+  "short_legs": shortLegs,
+  "wikipedia_url": wikipediaUrl == null ? null : wikipediaUrl,
+  "hypoallergenic": hypoallergenic,
+  "cat_friendly": catFriendly == null ? null : catFriendly,
+  "bidability": bidability == null ? null : bidability,
+};
+}
+
+class Weight {
+  Weight({
+    this.imperial,
+    this.metric,
+  });
+
+  String imperial;
+  String metric;
+
+  factory Weight.fromJson(Map<String, dynamic> json) => Weight(
+    imperial: json["imperial"],
+    metric: json["metric"],
+  );
 
   Map<String, dynamic> toJson() => {
-        'id': id,
-        'name': name,
-        'cfa_url': cfaUrl,
-        'temperament': temperament,
-        'origin': origin,
-        'country_codes': countryCodes,
-        'country_code': countryCode,
-        'description': description,
-        'life_span': lifeSpan,
-        'child_friendly': childFriendly,
-      };
+    "imperial": imperial,
+    "metric": metric,
+  };
 }
