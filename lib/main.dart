50a52eeb758fe70474d483bdb5a8872d3a6cbfac
samuell324 <chistov_artem@mail.ru>
2020-10-07 20:28:19 +0300

    move Future <List<CatData>> and List<CatData> to futureBuilder

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:practice_3/widgets/futureBuilder.dart';
-import 'models/catData.dart';
-import 'dart:convert';
-import 'dart:async';
-import 'package:flutter/foundation.dart';
-import 'package:http/http.dart' as http;
-
 
 void main() {
   runApp(AnimalsListScreen());
@@ -14,16 +8,6 @@ void main() {
 class AnimalsListScreen extends StatefulWidget {
   @override
   _AnimalListScreenState createState() => _AnimalListScreenState();
-}
-
-Future<List<CatData>> fetchCats(http.Client client) async {
-  final response = await client.get('https://api.thecatapi.com/v1/breeds/');
-  return compute(parseCat, response.body);
-}
-
-List<CatData> parseCat(responseBody) {
-  final parsed = jsonDecode(responseBody) as List;
-  return parsed.map<CatData>((json) => CatData.fromJson(json)).toList();
 }
 
 class _AnimalListScreenState extends State<AnimalsListScreen> {
--- lib/widgets/futureBuilder.dart
+++ lib/widgets/futureBuilder.dart
@@ -2,7 +2,19 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'catList.dart';
 import 'package:practice_3/models/catData.dart';
-import 'package:practice_3/main.dart';
+import 'dart:convert';
+import 'dart:async';
+import 'package:flutter/foundation.dart';
+
+Future<List<CatData>> fetchCats(http.Client client) async {
+  final response = await client.get('https://api.thecatapi.com/v1/breeds/');
+  return compute(parseCat, response.body);
+}
+
+List<CatData> parseCat(responseBody) {
+  final parsed = jsonDecode(responseBody) as List;
+  return parsed.map<CatData>((json) => CatData.fromJson(json)).toList();
+}
 
 class FutureBuilderClass extends StatefulWidget {
   @override
